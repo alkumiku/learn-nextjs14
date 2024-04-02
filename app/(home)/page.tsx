bc4004abb4d6bb0f6e4ca7e52d102f94101df94d
minho <seongmiku0917@gmail.com>
2024-04-03 02:07:53 +0900

    Change to server component & Add loading component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,26 +1,19 @@
-"use client";
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    try {
-      const response = await fetch(
-        "https://nomad-movies.nomadcoders.workers.dev/movies"
-      );
-      const json = await response.json();
-      setMovies(json);
-      setIsLoading(false);
-    } catch (err) {
-      console.error("Error fetching data:", err);
-      setIsLoading(false);
-    }
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
+async function getMovies() {
+  // return fetch(URL).then((response) => response.json());
 
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
